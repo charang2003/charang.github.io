29ab84ba0b2d30b1ee65f936e254c392275796d6
unknown <charang2003@gmail.com>
2024-08-19 13:04:40 +0530

    updated PageTransition file

--- components/ui/PageTransition.jsx
+++ components/ui/PageTransition.jsx
@@ -11,7 +11,8 @@ const PageTransition = ({ children }) => {
       <div key={pathname}>
         <motion.div
           initial={{ opacity: 1 }}
-          animate={{ opacity: 0, transition: { delay: 1, duration: 0.4, ease: "easeInOut" } }}
+          animate={{ opacity: 0, transition: { delay: 1, duration: 0.4, ease: "easeInOut" } 
+        }}
           className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
         />
           {children}
